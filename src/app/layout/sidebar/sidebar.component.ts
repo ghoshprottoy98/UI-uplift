6c7fadebd4f9919b362d4e3ae854bcfc14cf7f58
Pratya Ghosh <pratya.ghosh@bracits.com>
2024-12-12 13:38:36 +0600

    sidebar stays highlighted even after reloaded based on url

--- src/app/layout/sidebar/sidebar.component.ts
+++ src/app/layout/sidebar/sidebar.component.ts
@@ -1,13 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { LayoutService } from '../layout.service';
 import { Observable } from 'rxjs';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrl: './sidebar.component.scss'
+  styleUrls: ['./sidebar.component.scss']
 })
-export class SidebarComponent {
+export class SidebarComponent implements OnInit {
   isCollapsed$ = new Observable<boolean>();
   menuItems = [
     { label: 'Layouts', route: '/home/layouts' },
@@ -18,13 +19,25 @@ export class SidebarComponent {
   
   selectedLabel: string | null = null;
 
-  constructor(private layoutService: LayoutService) {}
+  constructor(private layoutService: LayoutService, private router: Router, private activatedRoute: ActivatedRoute) {}
 
   ngOnInit(): void {
     this.isCollapsed$ = this.layoutService.isCollapsed$;
+    this.setSelectedLabelFromRoute();
+    this.router.events.subscribe(() => {
+      this.setSelectedLabelFromRoute();
+    });
   }
 
   selectItem(label: string): void {
     this.selectedLabel = label;
+  }
+
+  private setSelectedLabelFromRoute(): void {
+    const currentUrl = this.router.url; 
+    const selectedItem = this.menuItems.find(item => currentUrl.startsWith(item.route));  
+    if (selectedItem) {
+      this.selectedLabel = selectedItem.label; 
+    }
   }
 }
